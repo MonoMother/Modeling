044342dc4de8d6e87a378db5bbe8a5f2644f43d3
MonoMother <NatkaKarp005@gmail.com>
2024-02-09 22:11:01 +0400

    1

---
+++ src/main/java/com/example/strashno/MainClass.java
@@ -0,0 +1,9 @@
+package com.example.strashno;
+import java.util.LinkedList;
+import java.util.Stack;
+public class MainClass {
+    LinkedList<String> entrance = new LinkedList<>();
+    LinkedList<String> exit = new LinkedList<>();
+    Stack<Var> varStack = new Stack<>();
+
+}
